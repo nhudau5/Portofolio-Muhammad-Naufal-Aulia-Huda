0243741fa0a02b30bced9ea77e8fb5e26102f7ae
Naufal <126181197+nhudau5@users.noreply.github.com>
2025-09-27 15:11:43 +0700

    Add files via upload

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,7 @@ module.exports = {
     },
     extend: {
       colors: {
-        primary: "#14b8a6",
+        primary: "#27DDF5",
         secondary: "#64748b",
         dark: "#0f172a",
       },
